b63cb55a6cbd906a8840e89378fed6fbe1ef7501
CircleCI <circleci@circleci>
2019-11-29 18:08:06 +0100

    docs(gh-pages): build and deploy site [skip ci]

--- build-deploy-site.sh
+++ build-deploy-site.sh
@@ -8,32 +8,46 @@ HUGO_VERSION=0.60.1
 HUGO_DIST_DIR=/tmp/hugo/v${HUGO_VERSION}
 HUGO_RELEASE=https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz
 
-if [[ ${last_commit} ]]; then
+
+build_and_deploy(){
   echo "Buildind Hugo site"
   rm -rf docs/*
   rm $HUGO_DIST_DIR && mkdir -p $HUGO_DIST_DIR
-
   git config --global user.email circleci@circleci
   git config --global user.name CircleCI
-# update hugo theme submodule
+  # update hugo theme submodule
   git submodule sync && git submodule update --init --recursive
-# install hugo and build
-  wget $HUGO_RELEASE -P $HUGO_DIST_DIR && \ 
+  # install hugo and build
+  wget $HUGO_RELEASE -P $HUGO_DIST_DIR && \
   (cd $HUGO_DIST_DIR; tar -xzvf $HUGO_DIST_DIR/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz) && \
   (cd $HUGO_SOURCE_DIR; sudo npm install) && \
   HUGO_ENV=production $HUGO_DIST_DIR/hugo -v -s $HUGO_SOURCE_DIR -d $HUGO_BUILD_DIR
   if [[ $? -eq 0 ]]; then
-    echo "Deploying site updates"	  
+    echo "Deploying site updates"
     git add docs
     git commit -m "docs(gh-pages): build and deploy site [skip ci]"
     git push
   else
     echo "Hugo build failed"
-  fi 
+  fi
   exit $?
+}
+
+if [ $# -eq 1 ]; then
+case $1 in
+    --force)
+      build_and_deploy
+    ;;
+    *)
+       echo "Unknown arg $1"
+    ;;
+esac
+fi
+
+if [[ ${last_commit} ]]; then
+  build_and_deploy
 else
   echo "Skipping site building, lastest commit message doesn't change path directory 'site/'"
 fi
 
 exit 0
-
