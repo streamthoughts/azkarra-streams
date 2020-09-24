2c174621054ee3eef403eb6794b3bd2e16205f94
Florian Hussonnois <florian.hussonnois@gmail.com>
2020-09-24 10:31:08 +0200

    update(all): update copyright header

--- azkarra-quickstart/azkarra-quickstart-java/src/main/resources/archetype-resources/src/main/java/Version.java
+++ azkarra-quickstart/azkarra-quickstart-java/src/main/resources/archetype-resources/src/main/java/Version.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2019 StreamThoughts.
+ * Copyright 2019-2020 StreamThoughts.
  *
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements. See the NOTICE file distributed with
@@ -16,6 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #set ( $package = $package.replaceAll("-", ".") )
 package ${package};
 
